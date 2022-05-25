8187660568eb295f34529f01ac08a485ac246b23
Luciano <lucianonicolasrivera8@gmail.com>
2022-05-25 14:36:40 -0300

    formulario de registro

--- pagina principal/script.js
+++ pagina principal/script.js
@@ -6,9 +6,16 @@ function openNav() {
   function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
   }
+
+// ABRIR MODAL LOGIN
   const boton = document.getElementById("btn");
   const form = document.getElementById("login");
   boton.addEventListener("click",() => {
        form.classList.toggle("mcollapsed");
   });
-  
+  // ABRIR MODAL REGISTER
+  const botonr = document.getElementById("btnr");
+  const formr = document.getElementById("register");
+  botonr.addEventListener("click",() => {
+    formr.classList.toggle("rcollapsed");
+});
