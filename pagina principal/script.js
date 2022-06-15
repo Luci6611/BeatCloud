ccdcca8a47dfb3b3aeda39b6b1f7febfca79aead
Luciano <lucianonicolasrivera8@gmail.com>
2022-06-15 09:49:47 -0300

    agregando el objeto del crud

--- pagina principal/script.js
+++ pagina principal/script.js
@@ -1,3 +1,72 @@
+const apiURL = "https:62a8f315ec36bf40bdb0cdde.mockapi.io/cancion";
+// CRUD
+
+// VARIABLES
+let newNombreCancion = document.getElementById("cancionnombre");
+let newAutor = document.getElementById("cancionautor");
+let newCancionURL = document.getElementById("cancionportada");
+let newFormSubir = document.getElementById("form-subir");
+let newSeccionGenero = document.getElementById("canciongenero")
+let contenedorCanciones = document.getElementById("contenedorcanciones");
+newFormSubir.addEventListener("click", (e) => {
+  e.preventDefault();
+});
+// // obtener cancion de la api
+// async function getCancion() {
+//   const response = await fetch(apiURL);
+//   let dato = await response.json();
+//   return dato;
+// }
+// RESETEO DE FORMULARIO
+function clearForm() {
+  newFormSubir.reset();
+}
+// CREAR OBJETO CANCION NUEVA
+function crearNuevaCancion() {
+  let newCancion = {
+    "nombre":newNombreCancion.value,
+    "autor":newAutor.value,
+    "url":newCancionURL.value,
+    "genero":newSeccionGenero.value,
+  };
+  return newCancion;
+  
+}
+async function posteoCancion(newCancion) {
+  const response = await fetch(apiURL, {
+    method: "POST",
+    body: JSON.stringify(newCancion.value),
+    headers: {
+      "Content-type": "application/json; charset=UTF-8",
+    },
+  })
+  .then((response) => response.json())
+  .then((json) => console.log(json));
+
+  return response;
+}
+function btnCreate(){
+  let cancion = crearNuevaCancion;
+  posteoCancion(cancion)
+  clearForm()
+}
+// CARGAR CANCIONES
+function cargarCanciones() {
+  let canciones = getcanciones();
+  canciones.then((response) => {
+   response.map((cancion) => {
+    cancion.nombre, cancion.autor, cancion.url,cancion.genero,cancion.id;
+    });
+  });
+}
+
+
+// TENGO QUE SEGUIR VIENDO ESTO HAY UN ERROR EN LOS NOMBRES O EN LA CARGA ALGO FALTA
+//carga inicial
+
+
+
+// ABRIR Y CERRAR NAV
 
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
@@ -40,41 +109,5 @@ const modalSubir = document.getElementById("form-subir");
 botonSubir.addEventListener("click",()=>{
   modalSubir.classList.toggle("form-subir-collapsed")
 })
-
-// CRUD
-const mockApiURL = "https://62a8f315ec36bf40bdb0cdde.mockapi.io/";
-// VARIABLES
-let newNombreCancion = document.getElementById("cancionnombre");
-let newAutor = document.getElementById("cancionautor");
-let newCancionURL = document.getElementById("cancionportada");
-let newFormSubir = document.getElementById("form-subir");
-let contentedorCanciones = document.getElementById("contenedorcanciones");
-newFormSubir.addEventListener("click", (e) => {
-  e.preventDefault();
-});
-// RESETEO DE FORMULARIO
-function clearForm() {
-  newFormSubir.reset();
-}
-// CREAR CANCION NUEVA
-function crearNuevaCancion() {
-  let nuevaCancion = {
-    nombre: newNombreCancion.value,
-    autor: newAutor,
-    portada: newCancionURL.value,
-  };
-  return nuevaCancion;
-}
-async function postUser(user) {
-  const response = await fetch(mockApiURL, {
-    method: "POST",
-    body: JSON.stringify(user),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-  })
-    .then((response) => response.json())
-    .then((json) => console.log(json));
-
-  return response;
-}
+//carga inicial
+loadUsers();
