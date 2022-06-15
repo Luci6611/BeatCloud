3651624cf586cb1fca17cd83a22b41e04434a934
Nicolas <nicolasmoralesj03@gmail.com>
2022-06-15 02:21:11 -0300

    mejoras y se agrega el carrito

--- pagina principal/script.js
+++ pagina principal/script.js
@@ -1,45 +1,44 @@
-
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-  }
-  
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+}
 
 // ABRIR MODAL LOGIN
-  const boton = document.getElementById("btn");
-  const form = document.getElementById("login");
-  boton.addEventListener("click",() => {
-       form.classList.toggle("mcollapsed");
-  });
-  // ABRIR MODAL REGISTER
-  const botonr = document.getElementById("btnr");
-  const formr = document.getElementById("register");
-  botonr.addEventListener("click",() => {
-    formr.classList.toggle("rcollapsed");
+const boton = document.getElementById("btn");
+const form = document.getElementById("login");
+boton.addEventListener("click", () => {
+  form.classList.toggle("mcollapsed");
+});
+// ABRIR MODAL REGISTER
+const botonr = document.getElementById("btnr");
+const formr = document.getElementById("register");
+botonr.addEventListener("click", () => {
+  formr.classList.toggle("rcollapsed");
 });
 
 // MODALES DE CRUD CONTROLS
 
 // ANIMACION DE CARRITO
 
-const cartButtons = document.querySelectorAll('.crud-card-btn');
+const cartButtons = document.querySelectorAll(".crud-card-btn");
 
-cartButtons.forEach(button => {
-	button.addEventListener('click', cartClick);
+cartButtons.forEach((button) => {
+  button.addEventListener("click", cartClick);
 });
 
 function cartClick() {
-	let button = this;
-	button.classList.add('clicked');
+  let button = this;
+  button.classList.add("clicked");
 }
 // animacion de boton flotante ABRIR MODAL DE SUBIR
 const botonSubir = document.getElementById("btnsubir");
 const modalSubir = document.getElementById("form-subir");
-botonSubir.addEventListener("click",()=>{
-  modalSubir.classList.toggle("form-subir-collapsed")
-})
+botonSubir.addEventListener("click", () => {
+  modalSubir.classList.toggle("form-subir-collapsed");
+});
 
 // CRUD
 const mockApiURL = "https://62a8f315ec36bf40bdb0cdde.mockapi.io/";
