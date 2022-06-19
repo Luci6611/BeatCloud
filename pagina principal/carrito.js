c0c65fb39cbb56169aa0bfa24873dcf9f7080bc7
Nicolas <nicolasmoralesj03@gmail.com>
2022-06-19 19:25:03 -0300

    se agregan los precios al carrito

--- pagina principal/carrito.js
+++ pagina principal/carrito.js
@@ -1,14 +1,15 @@
 let tablaCuerpo = document.getElementById("tablaCuerpo");
 let datos = [];
 
-const CapturarDatos = (titulo, portada) => {
+const CapturarDatos = (titulo, portada , precio1) => {
 
   const titulo1 = document.getElementById(titulo);
   const cardPortada = document.getElementById(portada);
   let autor = titulo1.nextElementSibling.innerHTML;
   let imagenPortada = cardPortada.src;
   let tituloCancion = titulo1.innerText;
-  datos.push({portada:imagenPortada , autor: autor,titulo: tituloCancion} );
+  let precio = document.getElementById(precio1).innerText;
+  datos.push({portada:imagenPortada , autor: autor,titulo: tituloCancion , precios:precio} );
   console.table(datos);
   LlenarCarrito();
 
@@ -23,6 +24,7 @@ const LlenarCarrito = () => {
         <th><img src="${dato.portada}" alt="portada" ></th>
         <th>${dato.autor}</th>
         <th>${dato.titulo}</th>
+        <th>${dato.precios}</th>
       `;
   let carrito =tablaCuerpo.appendChild(row);
   carrito.innerHTML=datosCarrito;
