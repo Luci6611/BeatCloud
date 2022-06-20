a8849d9638aae25ec49192959e7195ed873e63ae
Nicolas <nicolasmoralesj03@gmail.com>
2022-06-20 00:30:21 -0300

    se agregan mejoras en el carrito

--- pagina principal/carrito.js
+++ pagina principal/carrito.js
@@ -1,7 +1,10 @@
 let tablaCuerpo = document.getElementById("tablaCuerpo");
 let datos = [];
 
-const CapturarDatos = (titulo, portada , precio1) => {
+
+/* Funcion para capturar los datos de las cards */
+
+const CapturarDatos = (titulo, portada, precio1) => {
 
   const titulo1 = document.getElementById(titulo);
   const cardPortada = document.getElementById(portada);
@@ -9,31 +12,34 @@ const CapturarDatos = (titulo, portada , precio1) => {
   let imagenPortada = cardPortada.src;
   let tituloCancion = titulo1.innerText;
   let precio = document.getElementById(precio1).innerText;
-  datos.push({portada:imagenPortada , autor: autor,titulo: tituloCancion , precios:precio} );
+  datos.push({ portada: imagenPortada, autor: autor, titulo: tituloCancion, precios: precio });
   console.table(datos);
   LlenarCarrito();
 
 };
 
+/* Funcion para llenar el carrito */
 
 const LlenarCarrito = () => {
 
- let row = document.createElement("tr");
+  let row = document.createElement("tr");
   datos.map(dato => {
-      let datosCarrito = `
+
+    let datosCarrito = `
         <th><img src="${dato.portada}" alt="portada" ></th>
         <th>${dato.autor}</th>
         <th>${dato.titulo}</th>
-        <th>${dato.precios}</th>
+        <th>$ ${dato.precios}</th>
       `;
-  let carrito =tablaCuerpo.appendChild(row);
-  carrito.innerHTML=datosCarrito;
-  
+    let carrito = tablaCuerpo.appendChild(row);
+    carrito.innerHTML = datosCarrito;
   });
 
 
 };
 
+/* Funcion para vaciar el carrito */
+
 const VaciarCarrito = () => {
-    tablaCuerpo.innerHTML ='';
+  tablaCuerpo.innerHTML = '';
 };
